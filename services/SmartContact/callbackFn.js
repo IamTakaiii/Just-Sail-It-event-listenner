c3246e2f80b22ae571e49a51c841dc9560575617
PeacefulDogs <nipas.devs@gmail.com>
2023-02-18 13:33:39 +0700

    chore : change message when send to subscriber

--- services/SmartContact/callbackFn.js
+++ services/SmartContact/callbackFn.js
@@ -4,20 +4,23 @@ import { redis_publisher } from "../RedisPubSub/PubSub.js"
 export function create (from, id, name, event ) {
     console.log("Create Funding hook !!!")
     let createEvent = { from, id, name, event }
-    console.log(JSON.stringify(createEvent, null, 4))
-    redis_publisher.publish('project:create_done', name)
+    const message = JSON.stringify(createEvent, null, 4)
+    console.log(message)
+    redis_publisher.publish('project:create_done', message)
 }
 
 export function donate (from, to, value, event) {
     console.log("Donate Funding hook !!!")
     let donateEvent = {  from, to, value: ethers.utils.formatEther(value._hex), event  }
-    console.log(JSON.stringify(donateEvent, null, 4))
-    redis_publisher.publish('project:donate_done', `${from} to ${to} - ${ethers.utils.formatEther(value._hex)}`)
+    const message = JSON.stringify(donateEvent, null, 4)
+    console.log(message)
+    redis_publisher.publish('project:donate_done', message)
 }
 
 export function withdraw(from, _to, value, event) {
     console.log("Withdraw Funding hook !!!")
     let withdrawEvent = { from, value: ethers.utils.formatEther(value._hex), event }
-    console.log(JSON.stringify(withdrawEvent, null, 4));
-    redis_publisher.publish('project:withdraw_done', 'withdraw done')
+    const message = JSON.stringify(withdrawEvent, null, 4)
+    console.log(message)
+    redis_publisher.publish('project:withdraw_done', message)
 }
